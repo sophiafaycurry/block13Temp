529933786ce69faf79cf55de2d8f8a1319c9d565
Sophia <sophiafaycurry@gmail.com>
2025-03-19 18:00:15 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,80 @@
+// Complete the following functions to make our program work!
+
+/**
+ * Converts the given Fahrenheit temperature `f` to Celsius.
+ * @param {number} f temperature in °F
+ * @returns {number} temperature in °C
+ */
+function convertToCelsius(f) {
+  return (fahrenheit -32) * 5 / 9; 
+}
+
+/**
+ * | Temperature | Description |
+ * | ----------- | ----------- |
+ * | < 32        | "very cold" |
+ * | < 64        | "cold"      |
+ * | < 86        | "warm"      |
+ * | < 100       | "hot"       |
+ * | >= 100      | "very hot"  |
+ *
+ * @param {number} f temperature in °F
+ * @returns {string} the description from the table above corresponding to
+ * the given Fahrenheit temperature `f`
+ */
+function describeTemperature(f) {
+  let feeling; 
+  if (fahrenheit < 32) { 
+    feeling = "very cold";
+  }
+  else if (fahrenheit < 64) { 
+    feeling = "cold";
+  }
+  else if (fahrenheit < 86) {
+    feeling = "warm";
+  }
+  else if (farenheit < 100) {
+    feeling = "hot";
+  }
+  else if (fahrenheit >= 100) {
+    feeling = "very hot";
+  }
+  }
+
+/**
+ * @param {number} limit
+ * @returns {100} a random integer in the range [0, `limit`)
+ */
+function getRandomInt(limit) {
+  return Math.floor(Math.random() * limit); 
+}
+
+
+// -------------------- DO NOT CHANGE THE CODE BELOW ---------------------- //
+/**
+ * Converts the given temperature from Fahrenheit to Celsius,
+ * then alerts the user with a descriptive message.
+ * @param {number} f temperature in °F
+ */
+function parseFahrenheit(f) {
+  const c = convertToCelsius(f);
+  const description = describeTemperature(f);
+  const message = `${f}°F is ${c}°C. That is ${description}.`;
+  alert(message);
+}
+
+const fahrenheitPrompt =
+  "Please enter a number. We will convert that temperature from Fahrenheit to Celsius.";
+let f = prompt(fahrenheitPrompt);
+parseFahrenheit(+f);
+
+alert("Let's try that again.");
+f = prompt(fahrenheitPrompt);
+parseFahrenheit(+f);
+
+alert("Let's try some random temperatures.");
+f = getRandomInt(110);
+parseFahrenheit(f);
+
+f = getRandomInt(110);
+parseFahrenheit(f);
